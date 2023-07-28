de3ff1f4baf829a2557d014d21f15dc5dcb056bd
Nitish <111342262+IINJII@users.noreply.github.com>
2023-07-28 21:05:59 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3030;
+const port = 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`Example app listening on port ${port}`)
 })
